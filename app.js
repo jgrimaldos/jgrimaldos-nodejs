77216291ce9c653545bdb2e6e444915dc37f2a1b
jgrimaldos <jgrimaldos@gmail.com>
2013-05-19 11:12:42 -0500

    First Hello World

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	var body = 'Hello World!';
+	res.setHeader('Content-Type', 'text/plain');
+	res.setHeader('Content-Lenght', body.length);
+	res.end(body);
+});
+
+var port = 3000;
+app.listen(port);
+console.log('Server at port: ' + port);
